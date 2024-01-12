6303767b64fd51cd166e6720e604aae260e01f21
dcaric <dcaric@me.com>
2024-01-12 09:48:00 +0100

    first commit

---
+++ DoubleList/Node.cpp
@@ -0,0 +1,8 @@
+//
+//  Node.cpp
+//  DoubleList
+//
+//  Created by Dario Caric on 12.01.2024..
+//
+
+#include "Node.hpp"
---
+++ DoubleList/Node.hpp
@@ -0,0 +1,13 @@
+//
+//  Node.hpp
+//  DoubleList
+//
+//  Created by Dario Caric on 12.01.2024..
+//
+
+#ifndef Node_hpp
+#define Node_hpp
+
+#include <stdio.h>
+
+#endif /* Node_hpp */
